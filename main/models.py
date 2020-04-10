fe5cd32d02e8601a39bf2ae7c6d7289e14ed9389
этот девушка <eldesgonzales@users.noreply.github.com>
2020-04-10 10:20:08 +0800

    Add files via upload

--- main/models.py
+++ main/models.py
@@ -27,8 +27,8 @@ class Penalty(models.Model):
 	rulenum = models.IntegerField(null = True, default = 0)
 	status = models.CharField(max_length = 45)
 
-	def __str__(self):
-		return self.ipaddress
+	def __int__(self):
+		return self.id
 
 class Audit(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -39,3 +39,33 @@ class Audit(models.Model):
 
 	def __str__(self):
 		return self.sourceip
+
+
+# class PenaltyInfo(object):
+# 	def __init__(self, id, ipaddress, lastaccessed, penaltycount, rulenum, status):
+# 		super (PenaltyInfo, self).__init__()
+# 		self.id = id
+# 		self.ipaddress = ipaddress		  
+# 		self.lastaccessed = lastaccessed		
+# 		self.penaltycount = penaltycount    		
+# 		self.rulenum = rulenum      		
+# 		self.status = status      
+# 		self.ipaddress = ipaddress	
+		
+
+
+# class PenaltyManager(models.Manager):
+# 	# =============================================== ADMIN DASHBOARD ===================================================
+# 	# Displays Penalty DB with Blacklist DB details (domain)
+# 	def displayPenaltyDB():
+# 		arr = [] #set()
+# 		cur = conn.cursor()
+# 		cur.execute("select main_penalty.id, ipaddress, lastaccessed, penaltycount, rulenum, status from main_penalty, main_blacklist where main_penalty.id_blacklist_id = main_blacklist.id")
+# 		rows = cur.fetchall()
+
+# 		for row in rows:
+# 			p = PenaltyInfo(row[0], row[1], row[2], row[3], row[4], row[5])
+# 			arr.append(p) #.add
+
+# 		return arr
+
